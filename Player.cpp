c809b8e08dc8194b4294b3a396479cf18624f65f
Rev <oria2006@gmail.com>
2023-11-12 01:36:57 +0200

    14.1.12

--- Player.cpp
+++ Player.cpp
@@ -1,4 +1,9 @@
+#pragma once
+
 #include "Player.h"
+#include "utilities.h"
+#include <math.h>
+#include <iostream>
 
 Player::Player(char* name, int maxHP, int force){
     m_name = name;
@@ -28,4 +33,40 @@ Player& Player:: operator=(const Player& p){
     m_coins = p.m_coins;
     return *this;
 }
+ void Player::printInfo() const{
+    printPlayerInfo(m_name, m_level, m_force, m_HP, m_coins);
+
+}
+void Player::levelUp(){
+    m_level < 10 ? m_level++ : m_level;
+}
+const int Player::getLevel(){
+    return m_level;
+}
+void Player::buff(int force){
+    m_force += force;
+}
+void Player::heal(int health){
+    m_HP = std::min(m_maxHP ,m_HP + health);
+}
+void Player::damage(int damage){
+    m_HP = std::max(0, m_HP - damage);
+}
+void Player::addCoins(int amount){
+    m_coins += amount;
+}
+
+bool const Player::isKnockedOut(){
+    return m_HP == 0;
+}
+
+bool Player::pay(int amount){
+    if(m_coins >= amount){
+        m_coins -= amount;
+        return true;
+    }
+    std::cout << "Not enough coins" << std::endl;
+    return false;
+}
+
 
--- Player.h
+++ Player.h
@@ -8,4 +8,17 @@ public:
     ~Player();
 
     Player& operator=(const Player& p);
+
+    void printInfo() const;
+
+    //game functions
+    void levelUp();
+    const int getLevel();
+    void buff(int force);
+    void heal(int health);
+    void damage(int damage); 
+    void addCoins(int amount);
+    bool pay(int amount);
+
+    bool const isKnockedOut();
 };
